996d14a751c4ebb582412d18b762584ffa5b52e3
Omkar Varma <82939049+omkara96@users.noreply.github.com>
2023-06-19 21:17:44 +0530

    Update Files_splits3.py

--- Files_splits3.py
+++ Files_splits3.py
@@ -2,7 +2,7 @@ import boto3
 import csv
 import configparser
 import os
-
+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
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
