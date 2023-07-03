bbc815991299a68d847a1c552371e0abbb4e5c32
Omkar Varma <82939049+omkara96@users.noreply.github.com>
2023-07-03 19:04:12 +0530

    Create file_fix_new.py

---
+++ file_fix_new.py
@@ -0,0 +1,42 @@
+def fix_delimited_file(input_file, output_file, delimiter):
+    encodings = ['utf-8', 'shift_jis', 'cp932']  # Add more encodings if needed
+
+    for encoding in encodings:
+        try:
+            with open(input_file, 'r', encoding=encoding) as file:
+                lines = file.readlines()
+
+            fixed_lines = []
+            prev_line = lines[0].replace('\n', '').replace('\r', '')  # Initialize prev_line with the first line
+
+            for line in lines[1:]:
+                line = line.replace('\n', '').replace('\r', '')
+                words = line.split(delimiter)
+
+                if len(words) < 5:
+                    prev_line += line
+                else:
+                    fixed_lines.append(prev_line)
+                    prev_line = line
+
+            fixed_lines.append(prev_line)
+
+            with open(output_file, 'w', encoding=encoding) as file:
+                for line in fixed_lines:
+                    file.write(line + '\n')
+
+            print(f'Successfully processed the file with encoding: {encoding}')
+            return
+
+        except UnicodeDecodeError:
+            pass
+
+    print('Unable to process the file with any of the specified encodings.')
+
+
+# Usage example
+input_file_path = 'input.txt'  # Replace with your input file path
+output_file_path = 'output.txt'  # Replace with your desired output file path
+delimiter = '\t'  # Replace with the delimiter used in your file
+
+fix_delimited_file(input_file_path, output_file_path, delimiter)
