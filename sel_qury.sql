7c4d994c8828e13c74792cdc5f824feaa03ac49b
Omkar Varma <82939049+omkara96@users.noreply.github.com>
2023-08-03 20:44:50 +0530

    Update sel_qury.sql

--- sel_qury.sql
+++ sel_qury.sql
@@ -9,13 +9,16 @@ WHERE
     AND table_name IN ('your_table1', 'your_table2') -- Add more table names if needed
     AND column_name NOT IN (
         SELECT
-            column_name
+            attname
         FROM
-            information_schema.columns
+            pg_attribute AS attr
+            JOIN pg_class AS cls ON attr.attrelid = cls.oid
+            JOIN pg_namespace AS ns ON cls.relnamespace = ns.oid
         WHERE
-            table_schema IN ('your_schema1', 'your_schema2') -- Add more schema names if needed
-            AND table_name IN ('your_table1', 'your_table2') -- Add more table names if needed
-            AND is_identity = 'YES'
+            ns.nspname IN ('your_schema1', 'your_schema2') -- Add more schema names if needed
+            AND cls.relname IN ('your_table1', 'your_table2') -- Add more table names if needed
+            AND attnum > 0
+            AND attidentity = 'd'
     )
 GROUP BY
     table_schema,
