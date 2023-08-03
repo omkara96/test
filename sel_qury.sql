c3d433779c66b50bb66dacc9584281f2b9dc30d7
Omkar Varma <82939049+omkara96@users.noreply.github.com>
2023-08-03 20:38:14 +0530

    Update sel_qury.sql

--- sel_qury.sql
+++ sel_qury.sql
@@ -1,7 +1,7 @@
 SELECT
     table_schema,
     table_name,
-    STRING_AGG(column_name, ', ' ORDER BY ordinal_position) AS all_columns
+    LISTAGG(column_name, ', ') WITHIN GROUP (ORDER BY ordinal_position) AS all_columns
 FROM
     information_schema.columns
 WHERE
@@ -9,13 +9,13 @@ WHERE
     AND table_name IN ('your_table1', 'your_table2') -- Add more table names if needed
     AND column_name NOT IN (
         SELECT
-            identity_column
+            column_name
         FROM
-            pg_table_def
+            information_schema.columns
         WHERE
-            schemaname IN ('your_schema1', 'your_schema2') -- Add more schema names if needed
-            AND tablename IN ('your_table1', 'your_table2') -- Add more table names if needed
-            AND identity = 'YES'
+            table_schema IN ('your_schema1', 'your_schema2') -- Add more schema names if needed
+            AND table_name IN ('your_table1', 'your_table2') -- Add more table names if needed
+            AND is_identity = 'YES'
     )
 GROUP BY
     table_schema,
